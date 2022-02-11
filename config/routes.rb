330d15a36cc685bed23131fe8107c5c19ccb19f9
Shubham Soni <shubham.soni@theporter.in>
2022-02-11 20:49:37 +0530

    final commit

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   #to add a user to a group
   post "/group_users", to: "group_users#create"
 
-  #others
+  #others apis
   get "/balance_at_user_level/:id", to: "audits#get_balance_at_user_level"
   get "/balance_at_group_level/:id", to: "audits#get_balance_at_group_level"
   get "/transaction_date_wise/:id", to: "audits#get_transaction_date_wise"
